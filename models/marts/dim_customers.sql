4c256359170960bd8d00cded397903c1ae0d48d7
suellencabralmiranda@gmail.com <suellencabralmiranda@gmail.com>
2026-09-17 09:55:09 +0000

    added jaffle shop source yml file

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -12,7 +12,16 @@ orders as (
 
 customer_orders as (
 
-    select * from {{ ref('stg_jaffle_shop__customer_orders') }}
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
 
 ),
 
